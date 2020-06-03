b866deffc120666177797ae257e38685756ae5f8
Dylan Martin <dmart164@unix3.csc.calpoly.edu>
2020-06-03 16:09:51 -0700

    done

--- DiGraph.java
+++ DiGraph.java
@@ -81,14 +81,6 @@ public int[] topSort(){
    int[] result = new int[list.length];
    LinkedList<Integer> q = new LinkedList<Integer>();
    int[] indegrees = indegrees();
-   int[] cycle = new int[list.length];
-   for(int n = 0; n < indegrees.length; n++)
-   {
-      if(indegrees[n] == 0 || list[n].size() == 1)
-         cycle[n] = -1;
-      else
-         cycle[n] = 0;
-   }
    int u;
    for(u = 0; u < list.length; u++)
       if(indegrees[u] == 0)
@@ -108,24 +100,166 @@ public int[] topSort(){
             q.addLast(list[u].get(j));
      }
   }
-   int length = list.length;
-   for(int z= 0; z < list.length; z++){
-      i += cycle[z];
-      length += cycle[z];}
-//   System.out.println(i);
-//   System.out.println(length);
-   if(i != length)
+   if(i != list.length)
       throw new IllegalArgumentException();
    return result;
 }
 
 public void printTop(){
    int[] num = topSort();
-   for(int i : num)
-      System.out.print((i+1)+ " ");
-   System.out.println();
+   for(int i = 0; i < num.length-1; i++)
+      System.out.print((num[i]+1)+ ",");
+   System.out.println((num[num.length-1]+1));
    
 }
+
+private class VertexInfo{
+
+   private int distance;
+   private int parent;
+
+   public VertexInfo(int distance, int parent)
+   {
+      this.distance =distance;
+      this.parent = parent;
+
+   }
+
+}
+
+
+private VertexInfo[] BFS(int s){
+   LinkedList<Integer> q=new LinkedList<Integer>();
+   int N = list.length;
+   VertexInfo[] vinfo =new VertexInfo[N];
+   int u = 0;
+   for(u= 0; u < N; u++)
+   {
+      vinfo[u] = new VertexInfo(-1,-1);
+   }
+   vinfo[s].distance = 0;
+   q.addLast(s);
+   while(!(q.isEmpty()))
+   {
+      u = q.removeFirst();
+      for(int v : list[u])
+      {
+         if(vinfo[v].distance == -1)
+         {
+            vinfo[v].distance = vinfo[u].distance +1;
+            vinfo[v].parent = u;
+            q.addLast(v);
+         }
+     }
+  }
+//  for(int j = 0; j < N; j++)
+  //   System.out.println(j + ", " + vinfo[j].parent + "," + vinfo[j].distance);
+  return vinfo;
+}
+
+
+public boolean isTherePath(int from, int to){
+   VertexInfo[] vinfo = BFS(from -1);
+   if((vinfo.length > to-1) &&(vinfo[to-1].parent != -1 || from == to))
+      return true;
+   return false;
+}
+
+public int  lengthOfPath(int from, int to){
+   
+   VertexInfo[] vinfo = BFS(from -1);
+   int  length = 0;
+   int val = to-1;
+   if((vinfo.length > to-1) && (vinfo[to-1].distance == -1))
+      return -1;
+   else if((vinfo.length > to-1)){
+      while(val != from-1)
+      {
+         length++;
+         val = vinfo[val].parent;
+      }
+      return length;
+   }
+   else
+      return -1;
+}
+
+
+public void printPath(int from, int to){
+   VertexInfo[] vinfo = BFS(from -1);
+   String output = "";
+   int val = to-1;
+   if((vinfo.length > to-1) && (vinfo[to-1].distance == -1))
+      System.out.println("No Path");
+   else if((vinfo.length > to-1)){
+      while(val != from-1)
+      {
+         output = "->" + (val+1) + output;
+         val = vinfo[val].parent;
+      }
+      output = (val+1)+output;
+      System.out.println(output);
+   }
+   else
+      System.out.println("No Path");
+}
+
+private class TreeNode{
+   private int vnum;
+   private LinkedList<TreeNode> child;
+
+   public TreeNode(int vnum){
+   this.vnum = vnum;
+   this.child = new LinkedList<TreeNode>();
+   }
+}
+
+
+private TreeNode buildTree(int s){
+   VertexInfo[] vinfo = BFS(s-1);
+   TreeNode[] treenodes = new TreeNode[vinfo.length];
+   for(int j = 0; j < list.length;j++)
+   {
+      treenodes[j]= new TreeNode(j);
+   }
+   
+   for(int i =0; i < list.length; i ++)
+   {
+      if(vinfo[i].parent != -1)
+      {
+         int parent = vinfo[i].parent;
+         treenodes[parent].child.addFirst(treenodes[i]);
+      }
+   }
+   return treenodes[s-1];
+}
+
+public void printTree(int s){
+   TreeNode root = buildTree(s);
+   int distance = 0;
+   printRec(distance,root);
+
+}
+
+private void printRec(int distance, TreeNode root){
+   String spaces = "";
+   if(root == null)
+      System.out.println();
+   else
+   {
+      for(int k = 0; k < distance; k ++)
+         spaces += "   ";
+      System.out.println(spaces + (root.vnum +1));
+      for(int i = 0; i < root.child.size(); i++)
+      {
+         printRec(distance+1,root.child.get(i));  
+      
+      }
+
+   }
+
+}
+
 
 
 }
--- DiGraphTest.java
+++ DiGraphTest.java
@@ -19,6 +19,10 @@ public static void main(String args[])
    System.out.println("- vertex count (enter v)");
    System.out.println("- print graph (enter p)");
    System.out.println("- topologicalsort (enter t)");
+   System.out.println("- is there a path (enter i)");
+   System.out.println("- length of the path (enter l)");
+   System.out.println("- print the shortest path (enter s)");
+   System.out.println("- print the BFS tree (enter b)");
    System.out.println("- Quit (enter q)");
    String line;
    System.out.println("enter an operation: ");
@@ -36,6 +40,13 @@ public static void main(String args[])
             graph.addEdge(from,to);
             System.out.println("added edge from " +from + " to " +to);
             break;
+         case "b":
+            System.out.println("enter a source vertex:");    
+            from= in.nextInt();     
+            in.nextLine();
+            System.out.println("generated tree from: " +from+ "\n");
+            graph.printTree(from);
+            break;
          case "d":
             System.out.println("enter a from and to edge:");    
             from= in.nextInt();     
@@ -49,12 +60,34 @@ public static void main(String args[])
             System.out.println("number of edges: " +graph.edgeCount());
             break;
          
-         case "v":
-            System.out.println("number of vertices: " +graph.vertexCount());
+         case "i":
+            System.out.println("enter a from and to edge:");    
+            from= in.nextInt();     
+            to= in.nextInt();     
+            in.nextLine();
+            boolean Result = graph.isTherePath(from,to);
+            System.out.println("is there a path from " + from + " to " +to +": " + Result);
+            break;
+         
+         case "l":
+            System.out.println("enter a from and to edge:");    
+            from= in.nextInt();     
+            to= in.nextInt();     
+            in.nextLine();
+            int length = graph.lengthOfPath(from,to);
+            System.out.println("length of path from " +from + " to " +to + ": " + length);
             break;
          
          case "p":
             graph.print();
+            break;
+         
+         case "s":
+            System.out.println("enter a from and to edge:");    
+            from= in.nextInt();     
+            to= in.nextInt();     
+            in.nextLine();
+            graph.printPath(from,to);
             break;
          
          case "t":
@@ -63,17 +96,22 @@ public static void main(String args[])
                break;
             }
             catch(IllegalArgumentException e){
-               System.out.println("Cycle");
+               System.out.println("Error: Graph contains a Cycle");
                break;
             }
+         case "v":
+            System.out.println("number of vertices: " +graph.vertexCount());
+            break;
 
          default:
             System.out.println("invalid option");
       }
 
 
-
-   System.out.println("enter another operation: ");
+   if(line.equals("q"))
+      System.out.println("Good bye.");
+   else
+      System.out.println("\nenter another operation: ");
    }
 
 
